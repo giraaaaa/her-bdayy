c49c322ed7e0e7b197acfaec413b20df35863427
giraaaaa <kgirishkumarkai@gmail.com>
2022-02-02 04:47:01 +0530

    edit

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "anu" && b=="0802")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
